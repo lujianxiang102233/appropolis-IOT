b2ef869cdd60390580c708874e332e000487ff89
lujianxiang102233 <409627448@qq.com>
2018-09-26 17:40:26 +0800

    左侧功能实现一半，未加高亮

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,27 +1,61 @@
 <template>
-    <div class="home">
-        <el-container>
-        <el-header style="height: 64px">
-            <div class="login">
-              <img src="../assets/images/u5.png" alt="">
+    <el-container>
+    <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        background-color="#001529"
+        text-color="#9F9F8D"
+        active-text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose">
+      :collapse="isCollapse">
+        <div class="logo" style="height:64px">
+            <div class="in"></div>
+        </div>
+        <el-submenu index="1">
+            <template slot="title">
+            <img src="../assets/images/u59.png" alt="" id="u59" style=" height: 14px">
+            <span slot="title" style="color:#fff">权限管理</span>
+            </template>
+            <el-menu-item index="1-1" style="padding-left: 48px;">公司管理</el-menu-item>
+            <el-menu-item index="1-2" style="padding-left: 48px;">角色管理</el-menu-item>
+            <el-menu-item index="1-3" style="padding-left: 48px;">用户管理</el-menu-item>
+            <el-menu-item index="1-4" style="padding-left: 48px;">操作日志</el-menu-item>
+        </el-submenu>
+    </el-menu>
+    <el-container>
+        <el-header style="height: 64px;">
+            <div class="img">
+             <img src="../assets/images/u67.png" @click="collapse">
             </div>
-            <div class="title">
-              <img src="../assets/images/u67.png" alt="">
-              <span class="text">APPROPOLIS</span>
-              <span id="edit">变更</span>
-            </div>
+            <span class="text">APPROPOLIS</span>
+            <span id="edit">变更</span>
         </el-header>
-        <el-container>
-            <el-aside width="200px">Aside</el-aside>
-            <el-main>Main</el-main>
-        </el-container>
-        </el-container>
-    </div>
+        <el-main><router-view/></el-main>
+    </el-container>
+    </el-container>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      isCollapse: false
+    }
+  },
+  methods: {
+    collapse() {
+      this.isCollapse = !this.isCollapse
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+      console.log(111)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+      console.log(222)
+    }
+  }
 }
 </script>
 
@@ -29,60 +63,54 @@ export default {
 .el-container {
   height: 100%;
   .el-header {
-    color: #333;
-    text-align: center;
-    padding: 0;
-    height: 64px;
+    background-color: #fff;
     line-height: 64px;
-    .login {
+    font-size: 28px;
+    float: left;
+    width: 500px;
+    .img {
       height: 100%;
+      width: 77px;
       float: left;
-      width: 256px;
-      background-color: #C0CAD3;
-      line-height:64px;
-        img {
-          height: 26.5px;
-          vertical-align: middle;
-        }
-    }
-    .title {
-      background-color: #fff;
-      height: 64px;
-      line-height: 64px;
-      font-size: 28px;
-      float: left;
-      width: 500px;
+      cursor: pointer;
       img {
         width: 20px;
         height: 15px;
-        float: left;
         margin-top: 28px;
-        margin-left: 28px;
-      }
-      .text {
-        font-size: 28px;
-        color: #333;
-        float: left;
-        font-weight: bold;
-        margin-left: 27px;
-      }
-      #edit {
-        color:#18ABFF;
-        float: left;
-        margin-left: 33px;
-        font-size: 14px;
       }
     }
-  }
-  .el-aside {
-    background-color: rgb(84, 92, 100);
-    .el-menu {
-      width: 200px;
+    .text {
+      font-size: 28px;
+      color: #333;
+      float: left;
+      font-weight: bold;
+    }
+    #edit {
+      color: #18abff;
+      float: left;
+      margin-left: 33px;
+      font-size: 14px;
     }
   }
-  .el-main {
-    background-color: #e9eef3;
-    color: #333;
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 256px;
+    min-height: 400px;
+    .logo {
+      padding: 19px;
+      background-color: #c0cad3;
+      box-sizing: border-box;
+      .in {
+        width: 100%;
+        height: 100%;
+        background: url(../assets/images/u5.png) no-repeat center center;
+        background-size: contain;
+      }
+    }
+    .el-submenu {
+      #u59 {
+        margin-right: 10px;
+      }
+    }
   }
 }
 </style>

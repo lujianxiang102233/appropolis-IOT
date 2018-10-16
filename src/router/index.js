0eb7ecbe8b3f70c820724bf30ca37a8c78de93fa
renfengye <renfengye@appropolis.com.cn>
2018-10-16 12:04:11 +0800

    index

--- src/router/index.js
+++ src/router/index.js
@@ -12,45 +12,54 @@ import roleaccess from '@/components/companies/roleaccess'
 Vue.use(Router)
 
 let router = new Router({
-  routes: [{
-    path: '/login',
-    // name: 'login',
-    component: login
-  }, {
-    path: '/',
-    // name: 'home',
-    redirect: '/home'
-  }, {
-    path: '/home',
-    // name: 'home',
-    component: home,
-    children: [{
-        path: '/companies',
-        // name: 'companies',
-        component: companies
-      }, {
-        path: '/companyaccess',
-        // name: 'companyaccess',
-        component: companyaccess
-      }, {
-        path: '/role',
-        name: 'role',
-        component: role
-      }, {
-        path: '/user',
-        // name: 'user',
-        component: user
-      }, {
-        path: '/roleaccess',
-        component: roleaccess
-      },
-      {
-        path: '/logs',
-        name: 'logs',
-        component: logs
-      }
-    ]
-  }]
+  routes: [
+    {
+      path: '/login',
+      // name: 'login',
+      component: login
+    },
+    {
+      path: '/',
+      // name: 'home',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      // name: 'home',
+      component: home,
+      children: [
+        {
+          path: '/companies',
+          // name: 'companies',
+          component: companies
+        },
+        {
+          path: '/companyaccess',
+          // name: 'companyaccess',
+          component: companyaccess
+        },
+        {
+          path: '/role',
+          name: 'role',
+          component: role
+        },
+        {
+          path: '/user',
+          // name: 'user',
+          component: user
+        },
+        {
+          path: '/roleaccess',
+          component: roleaccess
+        },
+        {
+          path: '/logs',
+          // name: 'logs',
+          component: logs
+        }
+      ]
+    }
+  ]
 })
 router.beforeEach((to, from, next) => {
   let token = localStorage.getItem('token')

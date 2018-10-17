0179905664214d7103cb584b3e4acb2f3d28c14e
renfengye <renfengye@appropolis.com.cn>
2018-10-17 18:12:21 +0800

    Checkbox取消 删除不用代码

--- src/components/companies/logs.vue
+++ src/components/companies/logs.vue
@@ -1,104 +1,98 @@
 <template>
     <div class="logs">
-        <el-checkbox v-model="checked">
-            <el-breadcrumb separator = "/">
-                <el-breadcrumb-item>权限管理</el-breadcrumb-item>
-                <el-breadcrumb-item>操作日志</el-breadcrumb-item>
-            </el-breadcrumb>
-        </el-checkbox>
-        <el-form :inline="true" :model = "formInline" ref="formInline" class="clearfix role-form">
+        <el-breadcrumb separator = "/">
+            <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+            <el-breadcrumb-item>操作日志</el-breadcrumb-item>
+        </el-breadcrumb>
+        <el-form :inline = "true" :model = "formInline" ref="formInline" class="clearfix role-form">
           <div>筛选</div>
           <el-form-item label = "操作人">
-              <el-input size="mini" v-model = "formInline.userName" placeholder="请输入" class="user-form"></el-input>
+              <el-input
+                size="mini"
+                v-model = "formInline.userName"
+                placeholder="请输入"
+                class="user-form">
+              </el-input>
           </el-form-item>
           <el-form-item label = "行为分类">
               <el-select
-                :clearable="true"
-                size="mini"
-                v-model="formInline.type"
-                placeholder="请选择"
-                @change="selectChange">
+                :clearable = "true"
+                size = "mini"
+                v-model = "formInline.type"
+                placeholder = "请选择">
                 <el-option
-                  v-for="item in allOperationTypes"
+                  v-for = "item in allOperationTypes"
                   :key = "item.id"
-                  :label="item.value"
-                  :value="item.id">
+                  :label = "item.value"
+                  :value = "item.id">
                 </el-option>
               </el-select>
-              <!-- <el-autocomplete
-                  size="mini"
-                  class="inline-input"
-                  v-model="formInline.type"
-                  :fetch-suggestions="querySearch"
-                  placeholder="请选择"
-                  @select="handleSelect">
-              </el-autocomplete> -->
           </el-form-item>
           <el-form-item label = "操作时间">
               <el-date-picker
-                  size="mini"
+                  size = "mini"
                   @blur = "dateBlur"
-                  v-model="formInline.date"
-                  type="datetimerange"
-                  range-separator="至"
-                  start-placeholder="请选择"
-                  end-placeholder="请选择">
+                  v-model = "formInline.date"
+                  type = "datetimerange"
+                  range-separator = "至"
+                  start-placeholder = "请选择"
+                  end-placeholder = "请选择">
               </el-date-picker>
           </el-form-item>
           <el-form-item class="fr">
-              <el-button type="primary" size="mini" @click = "onSubmit">查询</el-button>
-              <el-button size="mini" @click = "onResetForm('formInline')">重置</el-button>
+              <el-button type = "primary" size="mini" @click = "onSubmit">查询</el-button>
+              <el-button size = "mini" @click = "onResetForm('formInline')">重置</el-button>
           </el-form-item>
         </el-form>
         <el-table
-            class="table"
-            :data="tableData"
-            :header-row-class-name="headerColor"
-            style="100%">
+            class = "table"
+            :data = "tableData"
+            :header-row-class-name = "headerColor"
+            style = "100%">
             <el-table-column
-                prop="loginName"
-                label="操作人"
-                align="center"
-                width="180">
+                prop = "loginName"
+                label = "操作人"
+                align = "center"
+                width = "180">
             </el-table-column>
             <el-table-column
-                prop="operationType"
-                label="行为分类"
-                align="center"
-                width="180">
+                prop = "operationType"
+                label = "行为分类"
+                align = "center"
+                width = "180">
             </el-table-column>
             <el-table-column
-                prop="operateDate"
-                label="操作时间"
-                align="center"
-                width="240">
+                prop = "operateDate"
+                label = "操作时间"
+                align = "center"
+                width = "240">
                 <template slot-scope="scope">
                   {{ scope.row.operateDate | time}}
                 </template>
             </el-table-column>
             <el-table-column
-                prop="loginIp"
-                label="IP地址"
-                align="center"
-                width="180">
+                prop = "loginIp"
+                label = "IP地址"
+                align = "center"
+                width = "180">
             </el-table-column>
             <el-table-column
-                prop="remark"
-                align="center"
-                label="行为描述">
-                <template slot-scope="scope">
-                  <div class="lastClass" :title="scope.row.remark">{{scope.row.remark}}</div>
+                prop = "remark"
+                align = "center"
+                label = "行为描述">
+                <template slot-scope ="scope">
+                  <div class ="lastClass" :title="scope.row.remark">{{scope.row.remark}}</div>
                 </template>
             </el-table-column>
         </el-table>
         <el-pagination
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            :current-page="currentPage"
-            :page-sizes="[10,20,50,100]"
-            :page-size="pageSize"
-            layout="total,prev,pager,next,sizes,jumper"
-            :total="totalAll">
+            @size-change = "handleSizeChange"
+            @current-change = "handleCurrentChange"
+            :current-page = "currentPage"
+            :page-sizes = "[10,20,50,100]"
+            :page-size = "pageSize"
+            layout ="total,prev,pager,next,sizes,jumper"
+            :total = "totalAll">
         </el-pagination>
     </div>
 </template>
@@ -112,8 +106,6 @@ export default {
       pageIndex: 1,
       pageSize: 10,
       allOperationTypes: [],
-      //   check
-      checked: false,
       //   form
       formInline: {
         userName: '',
@@ -141,10 +133,10 @@ export default {
     this.getTableData()
     // this.getData()
     //   .then(data => {
-    //     console.log(data)
+    //     console.log(111)
     //   })
-    //   .catch(error => {
-    //     console.log(error)
+    //   .catch(err => {
+    //     console.log(err)
     //   })
   },
   computed: {
@@ -154,30 +146,6 @@ export default {
     // }
   },
   methods: {
-    // from type
-    // querySearch(queryString, cb) {
-    //   let allOperationTypes = this.allOperationTypes
-    //   let results = queryString
-    //     ? allOperationTypes.filter(this.createFilter(queryString))
-    //     : allOperationTypes
-    //   cb(results)
-    // },
-    // createFilter(queryString) {
-    //   return allOperationTypes => {
-    //     return (
-    //       allOperationTypes.value
-    //         .toLowerCase()
-    //         .indexOf(queryString.toLowerCase()) === 0
-    //     )
-    //   }
-    // },
-    // handleSelect(item) {
-    //   this.operationTypeId = item.id
-    // },
-    selectChange(item) {
-      // console.log(this.formInline.type)
-      this.operationTypeId = item.id
-    },
     // form picker
     dateBlur() {
       if (!this.formInline.date) {
@@ -224,10 +192,8 @@ export default {
       return new Promise(function(resolve, reject) {
         this.axios(urlRest)
           .then(response => {
-            if (response.data.content.code !== 0) {
-              reject(response)
-            } else {
-              resolve(response)
+            if (response.data.content.code === 0) {
+              resolve()
             }
           })
           .catch(error => {
@@ -253,7 +219,7 @@ export default {
       let userName = this.formInline.userName
         ? `${this.formInline.userName}`
         : `{empUsername}`
-      let type = this.formInline.type ? `${this.formInline.type}` : '-1'
+      let type = this.formInline.type !== '' ? `${this.formInline.type}` : '-1'
       let start, end
       if (
         this.formInline.date &&
@@ -289,6 +255,7 @@ export default {
           console.log(error)
         })
     },
+    // 时间戳
     formatTime(t) {
       let d =
         t.getFullYear() +
